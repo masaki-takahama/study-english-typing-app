564ae17270a013c02452582738e0dd580562376d
masaki-takahama <101110111+masaki-takahama@users.noreply.github.com>
2022-04-27 01:31:50 +0900

    画像以外のデータの引き渡しに成功

--- app/controllers/phrases_controller.rb
+++ app/controllers/phrases_controller.rb
@@ -1,11 +1,25 @@
 class PhrasesController < ApplicationController
 
 def index
-  @phrases = Phrase.includes(:user)
   # あとでallからインクルードに変更して表示するフレーズもユーザーに関連するものだけを取得して表示するようにしたい
-  @phrase = Phrase.find(2)
-  @arr_json = @phrase.text.to_json.html_safe
+  @phrase = Phrase.find(3)
   # 画面表示用に１つだけフレーズを取得しているので、表示を整えて、タイピング機能つけたら@phrase = Phrase.find(1)は消す
+
+
+  @phrases = Phrase.includes(:user)
+  @users = []
+
+  @phrases.each do |phrase|
+    @users.push(phrase)
+  end
+
+
+
+
+
+
+
+
 end
 
 def new
@@ -18,14 +32,15 @@ def create
     redirect_to root_path
   else
     @phrase = Phrase.new(phrase_params)
-    render :new
+    redirect_to root_path
   end
 end
 
 private
 
 def phrase_params
-  params.permit(:text, :meaning, :tag_id, :image).merge(user_id: current_user.id)
+  params.require(:phrase).permit(:text, :meaning, :tag_id, :image).merge(user_id: current_user.id)
 end
 
 end
+
